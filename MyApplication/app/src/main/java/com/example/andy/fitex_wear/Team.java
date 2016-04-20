acd73229398008ec774bac203effc1716c39cef4
VC <Chen@VCs-MacBook-Pro.local>
2016-04-20 14:42:16 -0400

    completed team rank

--- MyApplication/app/src/main/java/com/example/andy/fitex_wear/Team.java
+++ MyApplication/app/src/main/java/com/example/andy/fitex_wear/Team.java
@@ -1,13 +1,20 @@
 package com.example.andy.fitex_wear;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class Team extends Activity {
+import java.util.ArrayList;
+
+public class Team extends Activity implements WearableListView.ClickListener {
 
     private TextView mTextView;
+    private WearableListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,57 @@ public class Team extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                lv = (WearableListView) stub.findViewById(R.id.listViewTeam);
+                lv.setAdapter(new MyAdapter(Team.this));
+                lv.setClickListener(Team.this);
             }
         });
+    }
+
+    @Override
+    public void onClick(WearableListView.ViewHolder viewHolder) {
+
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+
+    }
+
+
+    private static ArrayList<String> listItems;
+    static {
+        listItems = new ArrayList<String>();
+        listItems.add("Avengers    12,450");
+        listItems.add("Justice     11,900");
+        listItems.add("X-Men       11,324");
+        listItems.add("Fantastic   9,450");
+
+    }
+
+    private class MyAdapter extends WearableListView.Adapter {
+        private final LayoutInflater mInflater;
+
+        private MyAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new WearableListView.ViewHolder(
+                    mInflater.inflate(R.layout.custom_row, null));
+        }
+
+        @Override
+        public void onBindViewHolder(WearableListView.ViewHolder holder, int position) {
+            TextView view = (TextView) holder.itemView.findViewById(R.id.textView);
+            view.setText(listItems.get(position).toString());
+            holder.itemView.setTag(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return listItems.size();
+        }
     }
 }
